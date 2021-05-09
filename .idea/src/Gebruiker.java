f14ed784d50ce5c051e094f3764fc3a9d1e74731
BilalElhassani <dakar_ali9@hotmail.com>
2021-05-10 00:10:33 +0200

    test gemaakt

---
+++ .idea/src/Gebruiker.java
@@ -0,0 +1,26 @@
+public class Gebruiker {
+
+    private String contract;
+    private String werkgever;
+    private int filiaal;
+
+    public Gebruiker (String contract, String werkgever, int filiaal){
+        this.contract = contract;
+        this.werkgever = werkgever;
+        this.filiaal = filiaal;
+    }
+
+    public boolean toegelaten(String contractAntwoord, String werkgeverAntwoord, int filiaalAntwoord){
+        return true;
+    }
+
+    public String soortContract(int antwoordSoortContract){
+        String a = null;
+        return a;
+    }
+
+    public String loon(String contractAntwoord, String werkgeverAntwoord, int filiaalAntwoord, int antwoordSoortContract){
+        String a = null;
+        return a;
+    }
+}
